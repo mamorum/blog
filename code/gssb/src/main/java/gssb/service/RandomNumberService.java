308b7bc77b03946f47c1ed62f9d311cf057a0afc
mamorum <mamoru310@yahoo.com>
2017-02-18 15:56:49 +0900

    Update spring boot intro.

--- code/gssb/src/main/java/gssb/service/RandomNumberService.java
+++ code/gssb/src/main/java/gssb/service/RandomNumberService.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 @Service  // サービスクラスに付与。
 public class RandomNumberService {
 
-	// 整数0-9の乱数を返却。
-	public int zeroToNine() {
-		return (int) (Math.random() * 10);
-	}
+  // 整数0-9の乱数を返却。
+  public int zeroToNine() {
+    return (int) (Math.random() * 10);
+  }
 }
