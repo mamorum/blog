76bbda2c9d32711721368790c2a28f324d118deb
mamorum <mamoru310@yahoo.com>
2017-02-22 12:59:37 +0900

    Update spring boot intro.

--- code/gssb/src/main/java/gssb/App.java
+++ code/gssb/src/main/java/gssb/App.java
@@ -6,19 +6,19 @@ import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-// import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
 public class App {
 
-	private static final SimpleDateFormat format 
-					= new SimpleDateFormat("HH:mm:ss");
+  private static final SimpleDateFormat
+    fmt = new SimpleDateFormat("HH:mm:ss");
 	
   // 5秒ごとに実行されるメソッド。
-  // @Scheduled(fixedRate = 5000)
+  //@Scheduled(fixedRate = 5000)
   public void reportTime() {
-    System.out.println(format.format(new Date()));
+    System.out.println(fmt.format(new Date()));
   }
 	
   public static void main(String[] args) {
