b39d112c0a09e76a9ac63c0a1736768f800b4673
mamorum <mamoru310@yahoo.com>
2017-03-14 21:34:27 +0900

    Update mockito post of spring boot intro.

--- code/gssb-rdb/src/test/java/gssb/rdb/controller/JpaMemoControllerTest.java
+++ code/gssb-rdb/src/test/java/gssb/rdb/controller/JpaMemoControllerTest.java
@@ -9,24 +9,24 @@ import gssb.rdb.repository.MemoRepository;
 
 public class JpaMemoControllerTest {
 
-	@Test public void testCreate() {
-		
-		// 準備：テストデータ
-		Memo memo = new Memo();
-		memo.text = "テスト";
+  @Test public void testCreate() {
 
-		// 準備：リポジトリのモック（戻り値を設定）
-		MemoRepository repo = mock(MemoRepository.class);
-		when(repo.save(memo)).thenReturn(memo);
+    // 準備：テストデータ
+    Memo memo = new Memo();
+    memo.text = "テスト";
 
-		// 準備：テスト対象（リポジトリのモックを設定）
-		JpaMemoController controller = new JpaMemoController();
-		controller.repository = repo;
+    // 準備：リポジトリのモック（戻り値を設定）
+    MemoRepository repo = mock(MemoRepository.class);
+    when(repo.save(memo)).thenReturn(memo);
 
-		// 実行
-		Map<String, Memo> result = controller.create(memo);
+    // 準備：テスト対象（リポジトリのモックを設定）
+    JpaMemoController controller = new JpaMemoController();
+    controller.repository = repo;
 
-		// 検証
-		assertThat(result.get("memo").text).isEqualTo("テスト");
-	}
+    // 実行
+    Map<String, Memo> result = controller.create(memo);
+
+    // 検証
+    assertThat(result.get("memo").text).isEqualTo("テスト");
+  }
 }
